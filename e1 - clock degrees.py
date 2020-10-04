2b12cde636c490a839307552b7b129e8eb2f2d78
Ksieciu <30780278+Ksieciu@users.noreply.github.com>
2020-10-04 18:28:54 +0200

    Done exercise one

---
+++ e1 - clock degrees.py
@@ -0,0 +1,16 @@
+def clock_diff(hour, minutes):
+    if hour >= 12:
+        hour -= 12
+    hour_degrees = hour * 30 + 0.5 * minutes
+    minutes_degrees = 6 * minutes
+    degrees_diff = abs(hour_degrees - minutes_degrees)
+    if degrees_diff > 180:
+        degrees_diff = 360 - degrees_diff
+    return degrees_diff
+
+time = input("Write time in format proper format(ex. 00:00): ")
+hour, minutes = time.split(':')
+hour = int(hour)
+minutes = int(minutes)
+
+print(clock_diff(hour, minutes), "degrees")
